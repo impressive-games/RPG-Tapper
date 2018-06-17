972eee27068e5bb780b563d219e6a524da5e0b3e
Aleksandr <impressive-art@mail.ru>
2018-06-17 21:06:53 +0300

    Мусор

---
+++ DNS-TheGame/Assets/CircleBHV.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CircleBHV : MonoBehaviour {
+	public Text testSpeed;
+	float speed;
+	float stopper = -0.05f;
+
+	void Start () {
+		speed = Random.Range (4f,7f);
+	}
+
+	void Update () {
+		testSpeed.text = "Speed: " + speed;
+		speed += stopper;
+	}
+
+	void FixedUpdate () {
+		transform.Rotate (0, speed, 0);
+	}
+}
